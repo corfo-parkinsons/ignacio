8fd3d0261f4aafe53364993a37cb83e4973b551d
Sergio Lucero <sergiolucero@gmail.com>
2023-10-04 10:41:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,16 @@ from textlib import droplines
 from linelib import simple_recorder 
 ##############################################
 st.set_page_config(layout="wide")
-fuente = st.selectbox('Choose Mode/Elija modo', 
-                      ('CETRAM', 'inglés'))
-tab1, tab2 = st.tabs(['Grabación','Revisión'])
-with tab1:
+#fuente = st.selectbox('Choose Mode/Elija modo', ('CETRAM', 'inglés'))
+#tab1, tab2 = st.tabs(['Grabación','Revisión'])
+#with tab1:
+if True:
     st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
     for dropline in droplines(fuente):
         st.write(dropline+chr(10))
     simple_recorder(fuente)
-with tab2:
+#with tab2:
+else:
     st.header('Contents of cetram-felix/AUDIO')
     contents = s3_contents()  
     st.table(contents)
